cb37f67eb5e19af53b43e9bb1e873323eff255a9
Rebecca <beca.dance@gmail.com>
2022-05-29 14:41:58 -0400

    expense components

--- Udemy/React Complete Guide/expense-tracker/src/components/ExpenseItem.js
+++ Udemy/React Complete Guide/expense-tracker/src/components/ExpenseItem.js
@@ -1,8 +1,19 @@
 import React from 'react'
+import './ExpenseItem.css'
 
 function ExpenseItem() {
+    const expenseDate = new Date(2022,5,29)
+    const expenseTitle = 'car insurance'
+    const expenseAmount = 29847
+
   return (
-    <div>ExpenseItem</div>
+    <div className='expense-item'>
+        <div>${expenseDate}</div>
+        <div className='expense-item__description'>
+            <h2>${expenseTitle}</h2>
+            <div className='expense-item__price'>${expenseAmount}</div>
+        </div>
+    </div>
   )
 }
 
