ceb1bc2659bf431a44539eda658b785be958d7e4
Rebecca <beca.dance@gmail.com>
2022-06-02 19:32:38 -0400

    ctci q1.4 5/10

--- CtCI/Arrays and Strings/Q4_Palindrome.java
+++ CtCI/Arrays and Strings/Q4_Palindrome.java
@@ -1,6 +1,17 @@
-
+import java.util.*;
 
 public class Q4_Palindrome {
     //given s string, check if it can be a palindrome if rearranged.
     //for even strs, everything is mirrored and for odd strs, the middle char is the pivot
+    public static boolean isPalindromePermutation(String s) {
+        HashMap <Character, Integer> map = new HashMap<>();
+        for(int i = 0; i < s.length(); i++) {
+            map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0) +1);
+        }
+        int count=0;
+        for(char key: map.keySet()) {
+            count += map.get(key) %2;
+        }
+        return count <= 1;
+    }
 }
