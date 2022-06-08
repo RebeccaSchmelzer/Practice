49e1365ccb6394f56a4affc25f8bf4acfa9e5bde
Rebecca <beca.dance@gmail.com>
2022-06-08 08:26:00 -0400

    ctci 1.2 5/10

--- CtCI/Arrays and Strings/Q2_CheckPermutation.java
+++ CtCI/Arrays and Strings/Q2_CheckPermutation.java
@@ -1,9 +1,11 @@
+
+
 public class Q2_CheckPermutation {
     
     public static String sort(String s) {
-        char[] content = s.toCharArray();
-        java.util.Arrays.sort(content);
-        return new String(content);
+       char[] content = s.toCharArray();
+	   java.util.Arrays.sort(content);
+	   return new String(content);
     }
     public static boolean permutation(String s, String t){
         return sort(s).equals(sort(t));
