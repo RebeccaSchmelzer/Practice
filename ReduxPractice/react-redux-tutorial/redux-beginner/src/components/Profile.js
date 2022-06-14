75be8b467529154fc0fc4c5b5c4028d1136e56ec
Rebecca <beca.dance@gmail.com>
2022-06-13 20:18:03 -0400

    understanding reducers

---
+++ ReduxPractice/react-redux-tutorial/redux-beginner/src/components/Profile.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Profile() {
+  return (
+    <div><h1>Profile page</h1>
+    <p>name:</p>
+    <p>age:</p>
+    <p>email:</p>
+
+    </div>
+  )
+}
+
+export default Profile
--- ReduxPractice/react-redux-tutorial/redux-beginner/src/index.js
+++ ReduxPractice/react-redux-tutorial/redux-beginner/src/index.js
@@ -3,11 +3,27 @@ import ReactDOM from 'react-dom/client';
 
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {configureStore} from '@reduxjs/toolkit'
+//wrapper to allow access to the store, set store to ur store
+import { Provider } from 'react-redux';
+
+//create a store to contain all states that you want to change
+//configure store allows u to create the store, init it.
+//pass info that is a collection of reducers - built into redux 
+//store contains all reducers that will be used in the app
+const store = configureStore({
+  //set it to empty obj bc u dont have reducers yet
+  //reducer is a function that takes in some info about 
+  reducer: {}
+})
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Provider store={store}>
     <App />
+    </Provider>
   </React.StrictMode>
 );
 
