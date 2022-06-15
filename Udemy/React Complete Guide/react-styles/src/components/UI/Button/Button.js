eca3c8650b6ccee48bc53110397e37893a741e9f
Rebecca <beca.dance@gmail.com>
2022-06-14 20:24:34 -0400

    media queries

--- Udemy/React Complete Guide/react-styles/src/components/UI/Button/Button.js
+++ Udemy/React Complete Guide/react-styles/src/components/UI/Button/Button.js
@@ -14,7 +14,7 @@ import styled from 'styled-components'
 
 //wraps styles in generated classes and add them in the dev tools
 const Button = styled.button`
-
+    width: 100%;
     font: inherit;
     padding: 0.5rem 1.5rem;
     border: 1px solid #8b005d;
@@ -23,6 +23,9 @@ const Button = styled.button`
     box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
     cursor: pointer;
 
+    @media (min-width: 768px) {
+      width: auto;
+    }
   
   &:focus {
     outline: none;
