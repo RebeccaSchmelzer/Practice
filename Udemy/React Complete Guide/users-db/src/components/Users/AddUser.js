f6672458b9be4b238edae3d8b86edb8ae64900f3
Rebecca <beca.dance@gmail.com>
2022-06-16 11:10:30 -0400

    useState notes on AddUser

--- Udemy/React Complete Guide/users-db/src/components/Users/AddUser.js
+++ Udemy/React Complete Guide/users-db/src/components/Users/AddUser.js
@@ -2,8 +2,20 @@ import React from 'react'
 import Button from '../UI/Button'
 import Card from '../UI/Card'
 import styles from './AddUser.module.css'
+import { useState } from 'react'
 
 const AddUser = () => {
+
+    //var and setvar are used in array destructuring - js syntax
+    //usestate always returns an array with exactly 2 elements
+    //with this syntax were pulling elements out of that returned aray
+    //store them in sep constants
+    //first is the current state
+    //everytime the component re-renders (when state is updated)
+    //first constant holds latest state snapshot
+    //second is a function called to change the state anf trigger the re-render
+  const [enteredUsername, setEnteredUsername] = useState('')
+
     const adduserHandler = (event) => {
         //prevent the requent from being sent automatically, has to be an onsubmit
         event.preventDefault()
