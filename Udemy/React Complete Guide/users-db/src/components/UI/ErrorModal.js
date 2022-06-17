101ad95d696522a8c0f327a8ae1e57d5778feae8
Rebecca <beca.dance@gmail.com>
2022-06-16 20:16:28 -0400

    worked with portals

--- Udemy/React Complete Guide/users-db/src/components/UI/ErrorModal.js
+++ Udemy/React Complete Guide/users-db/src/components/UI/ErrorModal.js
@@ -3,12 +3,16 @@ import Button from './Button'
 
 import Card from './Card'
 import classes from './ErrorModal.module.css'
+import  ReactDOM  from 'react-dom'
 
-const ErrorModal = (props) => {
-  return (
-    <React.Fragment>
-        <div className={classes.backdrop} onClick={props.onConfirm}/>
-    <Card className={classes.modal}>
+//backdrop here bc ur only using it here
+const Backdrop = (props) => {
+    return <div className={classes.backdrop} onClick={props.onConfirm}/>
+}
+
+const ModalOverlay = (props) => {
+    return (
+        <Card className={classes.modal}>
         <header className={classes.header}>
             <h2>
                 {props.title}
@@ -23,6 +27,26 @@ const ErrorModal = (props) => {
             <Button onClick={props.onConfirm}>Okay</Button>
         </footer>
     </Card>
+    )
+}
+
+//use react-dom to make the portals
+//react-dom brings all the logic from react whether its state or something else
+//react doesnt care where u put the logic, react-dom helps u put it online
+
+//first is the component
+//second is where were going
+//moving html content about to be rendered to a diff place
+const ErrorModal = (props) => {
+  return (
+    <React.Fragment>
+        {ReactDOM.createPortal(<Backdrop onConfirm={props.onConfirm}/>, document.getElementById('backdrop-root'))}
+        {ReactDOM.createPortal(
+        <ModalOverlay 
+        title={props.title}
+        message={props.message}
+        onConfirm={props.onConfirm}/>, 
+        document.getElementById('overlay-root'))}
     </React.Fragment>
   )
 }
