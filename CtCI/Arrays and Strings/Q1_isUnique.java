e0d35f9cc5e462e7dcb3760f390e009a149fcbf2
Rebecca <beca.dance@gmail.com>
2022-06-20 08:28:42 -0400

    ctci 1.1 java 6/10

--- CtCI/Arrays and Strings/Q1_isUnique.java
+++ CtCI/Arrays and Strings/Q1_isUnique.java
@@ -18,17 +18,19 @@ public class Q1_isUnique {
 
 
 	public static boolean isUniqueChars(String str) {
-		if (str.length() > 128) {
+		if(str.length() > 128) {
 			return false;
 		}
+
 		boolean[] char_set = new boolean[128];
-		for(int i = 0; i < str.length(); i++) {
+		for (int i =0; i< str.length(); i++) {
 			int val = str.charAt(i);
-			if(char_set[val] == true) return false;
+			if(char_set[val]) return false;
 			char_set[val] = true;
 		}
 		return true;
-    }
+		
+	}
 
 }
 
