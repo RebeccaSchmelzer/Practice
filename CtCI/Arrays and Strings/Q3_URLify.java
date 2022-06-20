5d943c8e0798d601a8946f2798d0b65c57de54f8
Rebecca <beca.dance@gmail.com>
2022-06-20 08:46:11 -0400

    ctci 1.3 java 4/10

--- CtCI/Arrays and Strings/Q3_URLify.java
+++ CtCI/Arrays and Strings/Q3_URLify.java
@@ -2,6 +2,30 @@ public class Q3_URLify {
     
 
     //use char arrays bc java strs immutable
+    public static void URLify(char[] str, int length) {
+        int spaceCount = 0, newLen, i;
+        for(i = 0; i<str.length; i++) {
+            if(str[i] == ' ') {
+                spaceCount++;
+            }
+        }
+
+        newLen = length + spaceCount*2;
+        str[newLen] = '\0';
+        for(i = length -1; i >=0; i--) {
+            if(str[i] == ' ') {
+                str[newLen -1] = '0';
+                str[newLen -2] = '2';
+                str[newLen -3] = '%';
+                newLen = newLen-3;
+            }
+            else {
+                str[newLen - 1] = str[i];
+                newLen--;
+            }
+        }
+    
+    }
     
     
 }
