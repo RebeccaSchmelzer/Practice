8a94b72176e7604ac68eba58783c4a2988562847
Rebecca <beca.dance@gmail.com>
2022-06-26 12:46:44 -0400

    max-profit 5/10

--- Leetcode/blind-75/max-profit.py
+++ Leetcode/blind-75/max-profit.py
@@ -22,3 +22,16 @@ class Solution:
         return max_profit
 
 # you want the max profit from selling on one day and buying on another, te biggest difference in the array
+
+    def maxProf2(self, prices: List[int]) -> int:
+        l, r = 0, 1
+        maxP = 0
+
+        while r < len(prices):
+            if prices[l] < prices[r]:
+                profit = prices[r] - profit[l]
+                maxP = max(maxP, profit)
+            else:
+                l = r
+            r += 1
+        return maxP
