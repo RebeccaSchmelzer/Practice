891f8550754a072fdb4102b0287c5e4d2f71e039
Rebecca <beca.dance@gmail.com>
2022-06-26 12:28:41 -0400

    two sum 5/10

--- Leetcode/blind-75/two-sum.py
+++ Leetcode/blind-75/two-sum.py
@@ -3,9 +3,11 @@ from typing import List
 
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        hashmap = {}
-        for i in range(len(nums)):
-            complement = target - nums[i]
-            if complement in hashmap:
-                return [i, hashmap[complement]]
-            hashmap[nums[i]] = i
+        prevMap = {} # to map all previous values
+    
+        for i, n in enumerate(nums): # iterate thru the index and actual number, so i and n
+            diff = target-n #before adding to the map, check diff
+            if diff in prevMap: # check if its in the map
+                return [prevMap[diff], i] # if it is, return the index 
+            prevMap[n] = i # set the value n as the current index
+        
