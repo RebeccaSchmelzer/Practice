0fb0bc95d43694fd4b358d150b1c1f1185f0c2df
Rebecca <beca.dance@gmail.com>
2022-07-06 08:19:32 -0400

    143 headercartbutton reducer

--- Udemy/React Complete Guide/food-orderer/src/components/Layout/HeaderCartButton.js
+++ Udemy/React Complete Guide/food-orderer/src/components/Layout/HeaderCartButton.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import CartContext from '../../store/cart-context'
 import CartIcon from '../Cart/CartIcon'
 import classes from './HeaderCartButton.module.css'
 
@@ -8,14 +9,31 @@ import classes from './HeaderCartButton.module.css'
   - prop chains like this could be handled using CONTEXT
 */
 
+/*
+  143
+  - bring is context for cart, not provider tho!
+  - using useContext, this component will be reevaled by react whenever the context is changed
+    - changes when we update it in the cart provider component
+  - so now these two are connected
+  - dont add the items.length bc its only the amount, 3 sushis = 1 order of sushi
+  - use reduce() to get the amount of an array -> look into this
+*/
+
 const HeaderCartButton = (props) => {
+
+  const cartCtx = useContext(CartContext)
+
+  const numOfCartItems = cartCtx.items.reduce((curNum, item) => {
+    return curNum + item.amount
+  }, 0)
+
   return (
     <button className={classes.button} onClick={props.onClick}>
         <span className={classes.icon}>
             <CartIcon />
         </span>
         <span>Your Cart</span>
-        <span className={classes.badge}>3</span>
+        <span className={classes.badge}>{numOfCartItems}</span>
     </button>
   )
 }
