dd719b83950682b258416e98b1a7b4dba427c532
Rebecca <beca.dance@gmail.com>
2022-07-05 21:23:05 -0400

    search sorted array

---
+++ Leetcode/blind-75/search-rotated-array.py
@@ -0,0 +1,25 @@
+from ast import List
+
+
+class solution:
+    def search(self, nums: List[int], target: int) -> int:
+        l, r = 0, len(nums-1)
+
+        while l <= r:
+            m = (l + r) //2
+            if target == nums[m]:
+                return m
+            
+            # left
+            if nums[l] <= nums[m]: 
+                if target > nums[m] or target < nums[l]:
+                    l = m+1
+                else:
+                    r = m -1
+            #right
+            else:
+                if target < nums[m] or target > nums[r]:
+                    r + m -1
+                else:
+                    l = m + 1
+        return -1
