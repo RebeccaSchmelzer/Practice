cf1bc5cfefdc17f93c83a84d84275ee0c4dd970b
Rebecca <beca.dance@gmail.com>
2022-09-20 09:11:31 -0400

    ran npm install and 165 notes

--- Udemy/React Complete Guide/section-13-practice/src/components/User.js
+++ Udemy/React Complete Guide/section-13-practice/src/components/User.js
@@ -1,7 +1,27 @@
 import classes from './User.module.css';
+import { Component } from 'react'
 
-const User = (props) => {
-  return <li className={classes.user}>{props.name}</li>;
-};
+/**
+ * 165 - defining a class
+ * react calls render method, equivalent to return in functional component
+ * in functions, props is a param, in class no props
+ * need to iimport component from react  - and extend it from user
+ * bc of extends component - this is used with props in classes
+ * now we have the class based equivalent to the functional component
+ * a class can also render a functional component like users.js
+ */
+
+class User extends Component {
+  // constructor() {
+
+  // }
+  render() {
+    return <li className={classes.user}>{this.props.name}</li>;
+  }
+}
+
+// const User = (props) => {
+//   return <li className={classes.user}>{props.name}</li>;
+// };
 
 export default User;
