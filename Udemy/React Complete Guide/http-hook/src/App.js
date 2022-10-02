1cda1fc3af15f8f689d56677bff32963c5c4c9ff
Rebecca <beca.dance@gmail.com>
2022-10-02 16:05:16 -0400

    update

--- Udemy/React Complete Guide/http-hook/src/App.js
+++ Udemy/React Complete Guide/http-hook/src/App.js
@@ -1,43 +1,66 @@
-import React, { useEffect, useState } from 'react';
+import React, {  useEffect, useState } from 'react';
 
 import Tasks from './components/Tasks/Tasks';
 import NewTask from './components/NewTask/NewTask';
+import useHttp from './hooks/useHttp';
 
 function App() {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
+  //const [isLoading, setIsLoading] = useState(false);
+  //const [error, setError] = useState(null);
+  // const transformTasks = useCallback((tasksObj) => {
+  //   const loadedTasks = [];
+
+  //     for (const taskKey in tasksObj) {
+  //       loadedTasks.push({ id: taskKey, text: tasksObj[taskKey].text });
+  //     }
+
+  //     setTasks(loadedTasks);
+  // })
+  
+  const httpData = useHttp()
   const [tasks, setTasks] = useState([]);
 
-  const fetchTasks = async (taskText) => {
-    setIsLoading(true);
-    setError(null);
-    try {
-      const response = await fetch(
-        'https://react-http-6b4a6.firebaseio.com/tasks.json'
-      );
+  const {isLoading, error, sendRequest: fetchTasks} = httpData
 
-      if (!response.ok) {
-        throw new Error('Request failed!');
-      }
+  // const fetchTasks = async (taskText) => {
+  //   setIsLoading(true);
+  //   setError(null);
+  //   try {
+  //     const response = await fetch(
+  //       'https://movie-db-7e32c-default-rtdb.firebaseio.com/tasks.json'
+  //     );
 
-      const data = await response.json();
+  //     if (!response.ok) {
+  //       throw new Error('Request failed!');
+  //     }
 
-      const loadedTasks = [];
+  //     const data = await response.json();
 
-      for (const taskKey in data) {
-        loadedTasks.push({ id: taskKey, text: data[taskKey].text });
-      }
+  //     const loadedTasks = [];
 
-      setTasks(loadedTasks);
-    } catch (err) {
-      setError(err.message || 'Something went wrong!');
-    }
-    setIsLoading(false);
-  };
+  //     for (const taskKey in data) {
+  //       loadedTasks.push({ id: taskKey, text: data[taskKey].text });
+  //     }
+
+  //     setTasks(loadedTasks);
+  //   } catch (err) {
+  //     setError(err.message || 'Something went wrong!');
+  //   }
+  //   setIsLoading(false);
+  // };
 
   useEffect(() => {
-    fetchTasks();
-  }, []);
+    const transformTasks = (tasksObj) => {
+      const loadedTasks = [];
+  
+        for (const taskKey in tasksObj) {
+          loadedTasks.push({ id: taskKey, text: tasksObj[taskKey].text });
+        }
+  
+        setTasks(loadedTasks);
+    }
+    fetchTasks({url: 'https://movie-db-7e32c-default-rtdb.firebaseio.com/tasks.json'}, transformTasks);
+  }, [fetchTasks]);
 
   const taskAddHandler = (task) => {
     setTasks((prevTasks) => prevTasks.concat(task));
