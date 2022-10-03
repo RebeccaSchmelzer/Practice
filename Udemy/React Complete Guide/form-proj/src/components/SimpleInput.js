34179df44ecf4fec23ee7182f7392ce1e7e67491
Rebecca <beca.dance@gmail.com>
2022-10-03 18:58:56 -0400

    203

--- Udemy/React Complete Guide/form-proj/src/components/SimpleInput.js
+++ Udemy/React Complete Guide/form-proj/src/components/SimpleInput.js
@@ -3,13 +3,16 @@ import {  useState} from 'react'
 const SimpleInput = (props) => {
 
   const [enteredName, setEnteredName] = useState('')
-  const [enteredNameIsValid, setThat] = useState(true)
+  const [enteredNameIsValid, setThat] = useState(false)
+  const [enteredNameTouched, setThatTouched] = useState(false)
 
   const nameInputHandler = event => {
     setEnteredName(event.target.value)
   }
 
   const formSubHandler = event => {
+
+    setThatTouched(true)
     event.preventDefault()
 
     if (enteredName.trim() === '') {
@@ -21,14 +24,16 @@ const SimpleInput = (props) => {
     console.log(enteredName);
   }
 
-  const nameInputClasses = enteredNameIsValid ? 'form-control' : 'form-control invalid'
+  const nameInputisInvalid = !enteredNameIsValid && enteredNameTouched
+
+  const nameInputClasses = nameInputisInvalid ? 'form-control invalid' : 'form-control'
 
   return (
     <form onSubmit={formSubHandler}>
       <div className={nameInputClasses}>
         <label htmlFor='name'>Your Name</label>
         <input type='text' id='name' onChange={nameInputHandler} value={enteredName}/>
-      {!enteredNameIsValid && <p className='error-text'>Name must not be empty</p>}
+      {nameInputisInvalid && <p className='error-text'>Name must not be empty</p>}
       </div>
       <div className="form-actions">
         <button>Submit</button>
