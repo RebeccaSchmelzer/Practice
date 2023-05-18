6f5dbf7b0d0aa03aa32fec9bb5332587d0b5cfcc
Rebecca <beca.dance@gmail.com>
2023-05-17 22:33:24 -0700

    70% tutorial complete

--- typescript-full-tutorial/src/index.ts
+++ typescript-full-tutorial/src/index.ts
@@ -1,1 +1,17 @@
 console.log('typescript is here');
+
+class User {
+    email: string
+    name: string
+    city: string = '' //not in constructor
+    constructor(email: string, name: string) {
+        this.email = email
+        this.name = name
+    }
+}
+
+const rebecca = new User('r@c', 'rebecca')
+
+rebecca.city = 'vancouver'
+
+export {}
