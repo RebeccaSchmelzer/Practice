23741973145f2af663973fd83a64948d16dcc2c6
Rebecca <beca.dance@gmail.com>
2023-07-01 16:28:36 -0700

    animated mobile menu

--- portfolio-practice/some-css/easybank-landing-page-master/app/js/script.js
+++ portfolio-practice/some-css/easybank-landing-page-master/app/js/script.js
@@ -1,18 +1,27 @@
 const btnHamburger = document.querySelector('#btnHamburger');
+const body = document.querySelector('body')
 const header = document.querySelector('.header')
 const overlay = document.querySelector('.overlay')
+const fades = document.querySelectorAll('.has-fade')
 
 btnHamburger.addEventListener('click', function(){
     console.log('open ham');
 
     if(header.classList.contains('open')) { //close ham menu
+        body.classList.remove('noscroll')
         header.classList.remove('open')
-        overlay.classList.remove('fade-in')
-        overlay.classList.add('fade-out')
+        fades.forEach(function(element){
+            element.classList.remove('fade-in')
+            element.classList.add('fade-out')
+        })
     }
     else { //open ham menu
+        body.classList.add('noscroll')
         header.classList.add('open')
-        overlay.classList.remove('fade-out')
-        overlay.classList.add('fade-in')
+        fades.forEach(function(element){
+            element.classList.remove('fade-out')
+            element.classList.add('fade-in')
+        })
+    
     }
 });
