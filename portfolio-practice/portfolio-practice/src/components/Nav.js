abe471baabcc5025c3313f6212a66687aa54b63a
Rebecca <beca.dance@gmail.com>
2023-07-03 15:04:49 -0700

    nav bar opens

--- portfolio-practice/portfolio-practice/src/components/Nav.js
+++ portfolio-practice/portfolio-practice/src/components/Nav.js
@@ -1,13 +1,27 @@
 import React from 'react'
+import { useState } from 'react'
 
 //create one nav element per page?
 //or just hard code it?
 
 const Nav = () => {
+  //add a conditional element for the nav bar?
+  //add a class to the button so it expands
+  const [theNav, openNav] = useState(false)
+
+  const bigNav = () => {
+    openNav(true)
+  }
+
+  const navButton = document.querySelector('.nav-item')
+  navButton.addEventListener('click', function(){
+    navButton.classList.add('open')
+  })
+
   return (
     <div class="navigation">
       <nav>
-        <button className='nav-item'>
+        <button className='nav-item' onClick={bigNav}>
           <span className='nav-item-number'>00</span>
           <span className='nav-item-text'>Hello</span>
         </button>
