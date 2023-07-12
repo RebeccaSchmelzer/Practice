2e963062b94d1ad3273d65b9d52fcbe32cc51ef6
Rebecca <beca.dance@gmail.com>
2023-07-11 21:42:32 -0700

    broke it with firebase

---
+++ portfolio-practice/portfolio-practice/src/firebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAjm03LnsH39vtoCmPxyKeSInb3lZjhgY0",
+  authDomain: "connector-mentor.firebaseapp.com",
+  projectId: "connector-mentor",
+  storageBucket: "connector-mentor.appspot.com",
+  messagingSenderId: "734513272138",
+  appId: "1:734513272138:web:63ec799f982e7483fdddb3"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
