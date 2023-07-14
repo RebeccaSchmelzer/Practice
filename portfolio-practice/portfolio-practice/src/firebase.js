3f531e16828efe180dcf22a6245b282a37a9dce8
Rebecca <beca.dance@gmail.com>
2023-07-13 19:59:07 -0700

    tried to firebase

--- portfolio-practice/portfolio-practice/src/firebase.js
+++ portfolio-practice/portfolio-practice/src/firebase.js
@@ -1,18 +1,6 @@
 import firebase from 'firebase/app'
+import 'firebase/auth'
+import firebaseConfig from './firebaseConfig'
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
+firebase.initializeApp(firebaseConfig)// TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyAjm03LnsH39vtoCmPxyKeSInb3lZjhgY0",
-  authDomain: "connector-mentor.firebaseapp.com",
-  projectId: "connector-mentor",
-  storageBucket: "connector-mentor.appspot.com",
-  messagingSenderId: "734513272138",
-  appId: "1:734513272138:web:63ec799f982e7483fdddb3"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
--- portfolio-practice/portfolio-practice/src/firebaseConfig.js
+++ portfolio-practice/portfolio-practice/src/firebaseConfig.js
@@ -1,5 +1,4 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -13,5 +12,4 @@ const firebaseConfig = {
   appId: "1:734513272138:web:63ec799f982e7483fdddb3"
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+export default firebaseConfig
