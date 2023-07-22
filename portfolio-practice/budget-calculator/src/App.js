8dac62d758764b13fd631bc4104d813cb8061cf9
Rebecca <beca.dance@gmail.com>
2023-07-21 20:33:24 -0700

    yay nice and simple

--- portfolio-practice/budget-calculator/src/App.js
+++ portfolio-practice/budget-calculator/src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [number, setNumber] = useState('')
+
+  const calculate = (e) => {
+    e.preventDefault()
+    const myNum = number * 10
+    console.log(myNum);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <form onSubmit={calculate}>
+            <h1>Log in</h1>
+            <input type="text" name="" id="" placeholder='hours for the week' value={number} onChange={(e) => setNumber(e.target.value)}/>
+            <button type="submit">calculate</button>
+        </form>
     </div>
   );
 }
